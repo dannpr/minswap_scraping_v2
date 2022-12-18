063ab1633ffc205263702dfcd31e33b3c9fa9eef
danny ba <dannba0@gmail.com>
2022-12-18 17:55:27 +0100

    little change

--- get_price.sh
+++ get_price.sh
@@ -17,7 +17,7 @@ echo "Price: $token_price "'₳'>>minprice.txt
 if [ ! -s mindata.csv ]; then
 	echo "price,date" >mindata.csv
 else
-	echo $token_price','| tr -d '\n' >>mindata.csv
+	echo "$token_price,"| tr -d '\n' >>mindata.csv
 	date >>mindata.csv 
 fi
 
@@ -38,7 +38,7 @@ curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE"
 
 sqlite3 dbPrice.db <<EOF
 #create table if not exists minPrice (price text, date text);
-create table dbPrice (price text, date text);
+create table dbPrice (price text, date_time text);
 insert into dbPrice values ('$token_price', '$date');
 select * from dbPrice;
 EOF
