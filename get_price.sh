44e808fc836dbc2f02a4e6f266f7337d7e93a1e2
danny ba <dannba0@gmail.com>
2022-12-18 18:26:17 +0100

    new database

--- get_price.sh
+++ get_price.sh
@@ -36,11 +36,22 @@ URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
 curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE"
 
-sqlite3 dbPrice.db <<EOF
-#create table if not exists minPrice (price text, date text);
-create table dbPrice (price text, date_time text);
-insert into dbPrice values ('$token_price', '$date');
-select * from dbPrice;
+# Check if the "dbPrice" database exists
+if sqlite3 -list dbPrice.db '.databases' | grep -q 'dbPrice'; then
+	# The "dbPrice" database exists
+	# Connect to the database and insert a row
+	sqlite3 dbPrice.db <<EOF
+	INSERT INTO dbPrice (price, date_time) VALUES ('$token_price', '$date');
+	SELECT * FROM dbPrice;
 EOF
+else
+	# The "dbPrice" database does not exist
+	# Create the database and table, then insert a row
+	sqlite3 dbPrice.db <<EOF
+	CREATE TABLE dbPrice (price text, date_time text);
+	INSERT INTO dbPrice (price, date_time) VALUES ('$token_price', '$date');
+	SELECT * FROM dbPrice;
+EOF
+fi
 
 echo "Finito"
