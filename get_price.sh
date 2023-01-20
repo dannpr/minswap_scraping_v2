bc6462e9d6e7bd404e93efb1cd635da710e987d7
dannpr <dannba0@gmail.com>
2023-01-20 18:13:55 +0000

    test code

--- get_price.sh
+++ get_price.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 
-curl https://minswap.org/ > minpage.txt
+curl https://minswap.org/ > ./minpage.txt
 echo "Currently running ..."
-echo "This is the current  price of MIN" > minprice.txt
-echo " " >> minprice.txt
+echo "This is the current  price of MIN" > ./minprice.txt
+echo " " >> ./minprice.txt
 
 #Get price data
 token_name=$(cat minpage.txt | grep -Po '(?<=class="jet-listing-dynamic-field__content">).*?(?<=MIN)')
 token_name="${token_name:4}"
-echo "Name: $token_name">>minprice.txt
+echo "Name: $token_name">>./minprice.txt
 
 token_price=$(cat minpage.txt | grep -Poz '(?<=class="jet-listing-dynamic-field__content">).*?(?<=₳)' | awk '{print $1}')
-echo "Price: $token_price "'₳'>>minprice.txt
+echo "Price: $token_price "'₳'>>./minprice.txt
 
 if [ ! -s mindata.csv ]; then
-	echo "price,date" >mindata.csv
+	echo "price,date" >./mindata.csv
 else
-	echo "$token_price,"| tr -d '\n' >>mindata.csv
-	date >>mindata.csv 
+	echo "$token_price,"| tr -d '\n' >>./mindata.csv
+	date >>./mindata.csv 
 fi
 
-echo " ">>minprice.txt 
+echo " ">>./minprice.txt 
 
-date >>minprice.txt 
+date >>./minprice.txt 
 
 cat minprice.txt
 
@@ -36,22 +36,11 @@ URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
 curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE"
 
-# Check if the "dbPrice" database exists
-if sqlite3 -list dbPrice.db '.databases' | grep -q 'dbPrice'; then
-	# The "dbPrice" database exists
-	# Connect to the database and insert a row
-	sqlite3 dbPrice.db <<EOF
-	INSERT INTO dbPrice (price, date) VALUES ('$token_price', '$date');
-	SELECT * FROM dbPrice;
+# Connect to SQLite database and insert data
+sqlite3 dbPrice.db <<EOF
+CREATE TABLE IF NOT EXISTS dbPrice (price text, date text);
+INSERT INTO dbPrice (price, date) VALUES ('$token_price', '$date');
+SELECT * FROM dbPrice;
 EOF
-else
-	# The "dbPrice" database does not exist
-	# Create the database and table, then insert a row
-	sqlite3 dbPrice.db <<EOF
-	CREATE TABLE dbPrice (price text, date text);
-	INSERT INTO dbPrice (price, date) VALUES ('$token_price', '$date');
-	SELECT * FROM dbPrice;
-EOF
-fi
 
 echo "Finito"
